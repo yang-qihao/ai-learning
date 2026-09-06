70c77a75e1cff135e75cd39f49e7469b0ebf6ad4
yang-qihao <yang-qihao@users.noreply.github.com>
2026-09-06 17:42:32 +0800

    修复 Day3：修正打印对象与命名

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import student_utils
-class students:
+class Students:
     def __init__(self,name,score):
         self.name = name 
         self.score = score
@@ -10,12 +10,10 @@ class students:
             return "pass"
         else:
             return "not pass"
-s1 = students("yqh",99)
-s2 = students("ywh",55)
-s1.behave()
-s2.behave()
+s1 = Students("yqh",99)
+s2 = Students("ywh",55)
 print(s1.behave() , s1.is_pass())
-print(s1.behave() , s2.is_pass())
+print(s2.behave() , s2.is_pass())
 print(s1.name, s1.score, s1.is_pass())
 print(s2.name, s2.score, s2.is_pass())
 # student = [{"name" : "xiaoming", "score" : 99},{"name" : "xiaoya" ,"score" :93},{"name" :"xiaomao" ,"score" :200}]
