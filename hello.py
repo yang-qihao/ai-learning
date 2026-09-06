bb3e1cebf9703bbbf813a0a07715c9ee9d05f635
yang-qihao <yang-qihao@users.noreply.github.com>
2026-09-06 12:01:45 +0800

    Day1 学生成绩管理

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello")
+def high_score():
+     highest = 0
+     for s in student:
+          if int(s["score"]) > highest:
+             highest = s["score"]
+     return highest
+student = [{"name" : "xiaoming", "score" : 99},{"name" : "xiaoya" ,"score" :93},{"name" :"xiaomao" ,"score" :200}]
+sum = 0
+for s in student:
+    sum = sum + s["score"]
+avg = sum / len(student)
+print(sum)
+print(avg)
+print(high_score())    
