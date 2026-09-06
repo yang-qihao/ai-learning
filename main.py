946d6bb83040a5f2ef22351df85cbfaf408fb37b
yang-qihao <yang-qihao@users.noreply.github.com>
2026-09-06 17:38:29 +0800

    Day3 面向对象

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import student_utils
-student = [{"name" : "xiaoming", "score" : 99},{"name" : "xiaoya" ,"score" :93},{"name" :"xiaomao" ,"score" :200}]
-print("总分：" , student_utils.total(student))
-print("平均分：" , student_utils.avg(student))
-print("最高分：" , student_utils.high_score(student))
+class students:
+    def __init__(self,name,score):
+        self.name = name 
+        self.score = score
+    def behave(self):
+        return f"我是： {self.name} 分数: {self.score}"
+    def is_pass(self):
+        if self.score >= 60:
+            return "pass"
+        else:
+            return "not pass"
+s1 = students("yqh",99)
+s2 = students("ywh",55)
+s1.behave()
+s2.behave()
+print(s1.behave() , s1.is_pass())
+print(s1.behave() , s2.is_pass())
+print(s1.name, s1.score, s1.is_pass())
+print(s2.name, s2.score, s2.is_pass())
+# student = [{"name" : "xiaoming", "score" : 99},{"name" : "xiaoya" ,"score" :93},{"name" :"xiaomao" ,"score" :200}]
+# print("总分：" , student_utils.total(student))
+# print("平均分：" , student_utils.avg(student))
+# print("最高分：" , student_utils.high_score(student))
