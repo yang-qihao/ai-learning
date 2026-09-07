2d41c8273f4a4d7ec94dabbd470880c7f9459d8d
yang-qihao <yang-qihao@users.noreply.github.com>
2026-09-07 09:25:40 +0800

    忽略.idea配置目录，修正chat.py

--- chat.py
+++ chat.py
@@ -8,10 +8,21 @@ client = OpenAI(
     api_key=os.environ.get("DASHSCOPE_API_KEY"),   # 从"环境"里取 key，而不是写死
     base_url="https://ws-1ue1906kn7u4jwic.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
 )
+messages=[
+        {"role": "system", "content": "你是一个友好的助手，回答要简短。"}
+    ]
 
-response = client.chat.completions.create(
+print("开始对话！输入 quit 退出。")
+while True:
+    user_input = input("你：")
+    if user_input == "quit":
+        print("bye")
+        break
+    messages.append({"role": "user", "content": user_input})
+    response = client.chat.completions.create(
     model="qwen-plus",
-    messages=[{"role": "user", "content": "你好，用一句话介绍你自己"}],
-)
-
-print(response.choices[0].message.content)
+    messages=messages,
+   )
+    reply = response.choices[0].message.content
+    print("AI:", reply)
+    messages.append({"role": "assistant", "content": reply})
